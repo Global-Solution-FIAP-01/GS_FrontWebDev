e8b4909cb62217cc7434779ab37208f23e8dd5f9
Murilo Pina <murilo.mpina@gmail.com>
2025-11-10 10:56:43 -0300

    Criando estrutura do projeto

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>GS Frontend</h1>
+      <Modal />
+      <Card />
+    </>
+  )
+}
+
+export default App
