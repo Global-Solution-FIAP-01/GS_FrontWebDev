7d1d888c38b55856dba5ccff6ac4bafb967b6000
Murilo Pina <murilo.mpina@gmail.com>
2025-11-10 11:45:10 -0300

    Criando componentes e página principal

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from './components/Hero'
+import Description from './components/Description'
+import CardSection from './components/CardSection'
+import Footer from './components/Footer'
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <Description />
+      <CardSection />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
